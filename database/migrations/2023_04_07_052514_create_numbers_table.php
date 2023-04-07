86787c722c7d851be6681633d82ec3ecccf5d3aa
MInojan <inojanmaheswaran.zincat@gmail.com>
2023-04-07 13:03:12 +0530

    Add number column in migration file

--- database/migrations/2023_04_07_052514_create_numbers_table.php
+++ database/migrations/2023_04_07_052514_create_numbers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('numbers', function (Blueprint $table) {
             $table->id();
+            $table->string('number');
             $table->timestamps();
         });
     }
